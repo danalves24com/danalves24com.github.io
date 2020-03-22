8f4af19da91aca511183559941f64156b917428b
Daniel Rosel <danalves24@outlook.com>
2020-03-22 19:44:25 +0100

    alot

---
+++ scripts/fetch-response/script.js
@@ -0,0 +1,19 @@
+export default function getData(link) {
+    const opt = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/json',
+            "Access-Control-Allow-Origin": "*",
+        },
+        mode: 'cors',
+        cache: 'default'
+    }
+    var output = ""
+    fetch(link, opt).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log("Response: "+data)
+        output = data
+    })
+    return output
+}
