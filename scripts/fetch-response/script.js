5815aab31916c953b46d6b8c7a81c9aa57c41edf
Daniel Rosel <danalves24@outlook.com>
2020-03-22 22:18:29 +0100

    modular

--- scripts/fetch-response/script.js
+++ scripts/fetch-response/script.js
@@ -1,16 +1,23 @@
-    function getData(link) {
-        const opt = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'text/json',
-                "Access-Control-Allow-Origin": null,
-            },
-            mode: 'no-cors',
-            cache: 'default'
-        }        
-    fetch(link, opt).then((response) => {
+const defaultOPT = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'text/json',
+        "Access-Control-Allow-Origin": "*",
+    },
+    mode: 'cors',
+}    
+function getData(link, options) {
+    fetch(link, options)
+    .then((response) => {
         return response.json();
-    }).then((data) => {
+    })
+    .then((data) => {
         console.log(data)
     })
-    }
+    .catch(err => {
+        console.log(err);
+    })
+
+
+
+}
