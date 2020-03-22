51c6a8db187c2d7b8dc24b0cabd39e33e2d7bfd5
Daniel Rosel <danalves24@outlook.com>
2020-03-22 21:51:07 +0100

    accescontrolalloworigin link

--- scripts/fetch-response/script.js
+++ scripts/fetch-response/script.js
@@ -1,13 +1,14 @@
-    const opt = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'text/json',
-            "Access-Control-Allow-Origin": "*",
-        },
-        mode: 'cors',
-        cache: 'default'
-    }
+    const opt 
     function getData(link) {
+        opt = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'text/json',
+                "Access-Control-Allow-Origin": '"'+link+'"',
+            },
+            mode: 'cors',
+            cache: 'default'
+        }
     fetch(link, opt).then((response) => {
         return response.json();
     }).then((data) => {
