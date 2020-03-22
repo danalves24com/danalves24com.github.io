42834948f4661663921af5970227b9732324bc93
Daniel Rosel <danalves24@outlook.com>
2020-03-22 22:37:51 +0100

    formating

--- scripts/countries/script.js
+++ scripts/countries/script.js
@@ -4,76 +4,79 @@ countryOfCapital(capital) - find country by capital
 CountryCode(country code) - country by country code
 callingCode(calling code) - country by calling code
 `
+
 function allCountries() {
     fetch("https://restcountries-v1.p.rapidapi.com/all", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-		"x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
-	}
-})
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data)
-})
-.catch(err => {
-	console.log(err);
-});
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                "x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
 function countryOfCapital(capital) {
-    fetch("https://restcountries-v1.p.rapidapi.com/capital/"+capital, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-		"x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
-	}
-})
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data)
-})
-.catch(err => {
-	console.log(err);
-});
+    fetch("https://restcountries-v1.p.rapidapi.com/capital/" + capital, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                "x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
+
 function CountryCode(ccd) {
-    fetch("https://restcountries-v1.p.rapidapi.com/alpha/"+ccd, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-		"x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
-	}
-})
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data)
-})
-.catch(err => {
-	console.log(err);
-});
+    fetch("https://restcountries-v1.p.rapidapi.com/alpha/" + ccd, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                "x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
+
 function callingCode(cacd) {
-    fetch("https://restcountries-v1.p.rapidapi.com/callingcode/"+cacd, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-		"x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
-	}
-})
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data)
-})
-.catch(err => {
-	console.log(err);
-});
+    fetch("https://restcountries-v1.p.rapidapi.com/callingcode/" + cacd, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                "x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+            }
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
