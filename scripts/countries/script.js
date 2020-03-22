ac2f0f517f5bee1e59d72c9d481f20617151cb69
Daniel Rosel <danalves24@outlook.com>
2020-03-22 22:32:59 +0100

    callin code, country code

--- scripts/countries/script.js
+++ scripts/countries/script.js
@@ -1,8 +1,5 @@
 /*
 
-
-
-
 */
 function allCountries() {
     fetch("https://restcountries-v1.p.rapidapi.com/all", {
@@ -25,6 +22,42 @@ function allCountries() {
 
 function countryOfCapital(capital) {
     fetch("https://restcountries-v1.p.rapidapi.com/capital/"+capital, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+		"x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+	}
+})
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data)
+})
+.catch(err => {
+	console.log(err);
+});
+}
+function CountryCode(ccd) {
+    fetch("https://restcountries-v1.p.rapidapi.com/alpha/"+ccd, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+		"x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+	}
+})
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data)
+})
+.catch(err => {
+	console.log(err);
+});
+}
+function callingCode(cacd) {
+    fetch("https://restcountries-v1.p.rapidapi.com/callingcode/"+cacd, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
