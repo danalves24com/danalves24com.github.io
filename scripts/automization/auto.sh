d37ca981d79221e67f4d10c5891e2bca1f858ec8
Daniel Rosel <danalves24@outlook.com>
2020-03-23 12:43:31 +0100

    2271

--- scripts/automization/auto.sh
+++ scripts/automization/auto.sh
@@ -2,7 +2,7 @@
 git checkout dev
 for (( c=1; c<=10; c++ ))
 do
-	echo "$c is the index rn" >> info.json
+	echo "$c is the number in the array" >> info.json
 	git add .
 	git commit -m $RANDOM
 	git push && git pull
