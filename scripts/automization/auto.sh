9e67ca73567aaf65f18f5a412eb8b3e78dcb2ca2
Daniel Rosel <danalves24@outlook.com>
2020-03-23 12:41:10 +0100

    4991

--- scripts/automization/auto.sh
+++ scripts/automization/auto.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "2 is the index rn" >> info.json
-git add .
-git commit -m "upload"
-git push && git pull
-git checkout master
-git merge dev
+git checkout dev
+for (( c=1; c<=10; c++ ))
+do
+	echo "$c is the index rn" >> info.json
+	echo "2 is the index rn" >> info.json
+	git add .
+	git commit -m $RANDOM
+	git push && git pull
+	git checkout master
+	git merge dev
+done
