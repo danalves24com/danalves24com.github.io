16b6d222fa11877f1e002352230b6fe964f25dd6
Daniel Rosel <danalves24@outlook.com>
2020-04-12 14:15:21 +0200

    22206

---
+++ data/cookbook-api/create.sh
@@ -0,0 +1,18 @@
+clear
+ttrsc = []
+echo "numeber:"
+read number
+echo "Name:"
+read name
+echo "resources:"
+read rsc
+while(rsc != "X") do:
+read rsc
+ttrsc += rsc
+done
+echo $number
+echo $name
+data= `
+
+
+`
